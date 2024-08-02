e4a9bb5e522ba3222b794d14c61e434c5a978ba2
ajushrajiv <152269571+ajushrajiv@users.noreply.github.com>
2024-08-02 20:31:41 +0200

    ansible-nginx-docker

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+resource "aws_security_group" "example_sg" {
+  name        = "example_sg"
+  description = "Security group for SSH and HTTP access"
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-01e444924a2233b07"
+  instance_type = "t2.nano"
+  subnet_id     = aws_subnet.example_subnet.id
+  key_name      = "test-keypair"
+  tags = {
+    Name = "ansible-test"
+  }
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+}
+
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+}
+
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+}
+
+resource "aws_route_table_association" "example_route_table_assoc" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_route_table.id
+}
+
+
